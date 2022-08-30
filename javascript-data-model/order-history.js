2cdbd68bdf794a09b03e22e323240a1dae30f630
Brandon Au <design.brandonau@gmail.com>
2022-08-30 17:08:45 +0000

    added order#. total cost, img info

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,31 +1,43 @@
 var orderHistory = [
   {
+    ordernumber: '114-3941689',
     delivered: '08082020',
     item: 'Javascript for impatiant programmers',
+    image: './images/bookcover.png',
     author: 'Raushmayer, Dr. Axel',
     returnby: '09082020',
-    cost: '31.55'
+    cost: '31.55',
+    total: '34.00'
   },
   {
+    ordernumber: '113-9984268-1280257',
     delivered: '07202020',
     item: 'The Timeless Way of Building',
+    image: './images/bookcover.png',
     author: 'Alexander, Christopher',
     returnby: '08192020',
-    cost: '41.33'
+    cost: '41.33',
+    total: '44.53'
   },
   {
+    ordernumber: '114-2875557-9059409',
     delivered: '07072020',
     item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    image: './images/controller.png',
     author: null,
     returnby: '08052020',
-    cost: '15.98'
+    cost: '15.98',
+    total: '17.22'
   },
   {
+    ordernumber: '113-2883177-2648248',
     delivered: '07052020',
-    item: ['Gamecube Controller - Super Smash BRos. Edition (Nintendo Switch)', 'The Art of Sql'],
+    item: ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    image: ['./images/controller.png', './images/bookcover.png'],
     author: [null, 'Faroult, Stephane'],
     returnby: ['08042020', '08042020'],
-    cost: ['94.95', '33.99']
+    cost: ['94.95', '33.99'],
+    total: '138.93'
   }
 ];
 
