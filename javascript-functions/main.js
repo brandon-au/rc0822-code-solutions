b404d463551d3b5b0ddb464eb221517461cd8828
Brandon Au <design.brandonau@gmail.com>
2022-08-30 21:02:04 +0000

    corrected function getLastElement to return last element in the array

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -23,17 +23,13 @@ function getFirstName(person) {
   var firstName = person.firstName;
   return firstName;
 }
-var person = {
-  firstName: 'Lelouche',
-  lastName: 'Lamperouge'
-};
-getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
-console.log('value of getFirstName:', person.firstName);
+var getFirstNameResult = getFirstName({ firstName: 'Lelouch', lastName: 'Lamerouge' });
+console.log('value of getFirstName:', getFirstNameResult);
 
-function getLastElement(array) {
-  var lastElement = array.length - 1;
+function getLastElement(array) { // not actually an array, just the name of the parameter
+  var lastIndex = array.length - 1; // lastIndex variable is being assigned the last index in the variable array
+  var lastElement = array[lastIndex]; // lastElement variable is being assigned element at the last index (variable lastIndex) in the array that would be used in the argument
   return lastElement;
 }
-var array = ['propane', 'and', 'propane', 'accessories'];
-getLastElement(array);
-console.log('value of getLastElement:', array[array.length - 1]);
+var getLastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('value of getLastElement:', getLastElementResult);
