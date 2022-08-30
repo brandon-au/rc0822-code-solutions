95e504189445eceeec009cd5b61e8166f363c295
Brandon Au <design.brandonau@gmail.com>
2022-08-30 16:50:36 +0000

    Completed javascript-data-model exercise - Brandon Au

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,32 @@
+var orderHistory = [
+  {
+    delivered: '08082020',
+    item: 'Javascript for impatiant programmers',
+    author: 'Raushmayer, Dr. Axel',
+    returnby: '09082020',
+    cost: '31.55'
+  },
+  {
+    delivered: '07202020',
+    item: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnby: '08192020',
+    cost: '41.33'
+  },
+  {
+    delivered: '07072020',
+    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: null,
+    returnby: '08052020',
+    cost: '15.98'
+  },
+  {
+    delivered: '07052020',
+    item: ['Gamecube Controller - Super Smash BRos. Edition (Nintendo Switch)', 'The Art of Sql'],
+    author: [null, 'Faroult, Stephane'],
+    returnby: ['08042020', '08042020'],
+    cost: ['94.95', '33.99']
+  }
+];
+
+console.log('value of orderHistory:', orderHistory);
