1a725345a230db9345a8c651a9226713bcd3a6f4
Brandon Au <design.brandonau@gmail.com>
2022-10-26 22:01:17 +0000

    Completed react-function-components exercise - Brandon Au

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function CustomButton(props) {
+  return <button>Click Me!</button>;
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+const element = <CustomButton />;
+root.render(element);
+
+// function CustomButton(props) {
+//   return <button>Click Me!</button>;
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const element = <CustomButton />;
+// root.render(element);
