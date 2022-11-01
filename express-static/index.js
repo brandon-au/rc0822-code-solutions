3f06a70cda239f751a918d9d9bc05c1733ca67c8
Brandon Au <design.brandonau@gmail.com>
2022-11-01 17:42:41 +0000

    added console log to app.listen to confirm it works properly

--- express-static/index.js
+++ express-static/index.js
@@ -9,4 +9,6 @@ console.log('publicPath:', publicPath);
 app.use(express.static('publicPath'));
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000');
 });
