9a77f8f718822b8519761ca4c9512e3578c6233e
Brandon Au <design.brandonau@gmail.com>
2022-11-01 17:20:39 +0000

    updated argument in express-static to assigned const

--- express-static/index.js
+++ express-static/index.js
@@ -6,7 +6,7 @@ const app = express();
 const publicPath = path.join('__dirname', 'public');
 console.log('publicPath:', publicPath);
 
-app.use(express.static('public'));
+app.use(express.static('publicPath'));
 
 app.listen(3000, () => {
 });
